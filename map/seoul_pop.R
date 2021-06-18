8f22cb1e1245379ea5a2c46cc24f3c56476091f9
Moonster <msj5589@gmail.com>
2021-06-18 15:02:21 +0900

    map update
    visualize population in Seoul

---
+++ map/seoul_pop.R
@@ -0,0 +1,73 @@
+
+rm(list = ls())
+gc(reset = T)
+
+if(!require(dplyr)) install.packages('dplyr'); require(dplyr)
+if(!require(data.table)) install.packages('data.table'); require(data.table)
+if(!require(ggplot2)) install.packages('ggplot2'); require(ggplot2)
+if(!require(sf)) install.packages('sf'); require(sf)
+if(!require(httr)) install.packages('httr'); require(httr)
+if(!require(rvest)) install.packages('rvest'); require(rvest)
+
+setwd('Documents/GitHub/Rtips/map/')
+
+#----  MAC ggplot2 한글 깨짐 방지 ----#
+if(!require(extrafont)) install.packages('extrafont'); require(extrafont)
+theme_set(theme_gray(base_family='NanumGothic'))
+#-------------------------------------#
+
+#### Visualization .SHP file ####
+# link: https://data.seoul.go.kr/dataVisual/seoul/seoulLivingPopulation.do
+mapp = st_read("행정구역.shp") 
+head(mapp)
+p = ggplot(mapp) +
+  geom_sf(aes(fill = SIGUNGU_NM)) ## 서울시 지도, 자치구별 색상 다르게
+p
+
+# 행정구별 코드 정보 http://data.seoul.go.kr/dataVisual/seoul/seoulLivingPopulation.do
+sdng_df = fread('sdng_cd.csv') 
+sdng_df = sdng_df %>% 
+  mutate(H_SDNG_CD = as.character(H_SDNG_CD),
+         H_DNG_CD = as.character(H_DNG_CD))
+
+
+#### 유동인구 API ####
+# link: http://data.seoul.go.kr/dataList/OA-14991/S/1/datasetView.do
+get_df_fun = function(date_val_, tz_val_, sdng_df_, api_key_)
+{
+  url = paste0('http://openapi.seoul.go.kr:8088/', api_key,
+               '/xml/SPOP_LOCAL_RESD_DONG/',
+               1, '/',
+               500, '/', 
+               date_val, '/',
+               tz_val, '/')
+  tmp_lines = readLines(url)
+  df = lapply(c('stdr_de_id', 'tmzon_pd_se', 'adstrd_code_se', 'tot_lvpop_co'), function(x)
+    read_html(paste0(tmp_lines, collapse = '')) %>%
+      html_nodes('body row') %>% html_nodes(x) %>% html_text()) %>% 
+    do.call('cbind', .) %>%
+    data.frame()
+  colnames(df) = c('date', 'tz', 'H_DNG_CD', 'TOT')
+  return(df %>% left_join(sdng_df, by = 'H_DNG_CD'))
+}
+
+vis_fun = function(mapp, date_val, tz_val, path = getwd(), sdng_df = sdng_df, api_key = api_key)
+{
+  df = get_df_fun(date_val_ = date_val, tz_val_ = tz_val, sdng_df_ = sdng_df, api_key_ = api_key)
+  vis_df = mapp %>% left_join(df, by = c('ADM_CD' = 'H_SDNG_CD'))
+  g = ggplot(vis_df) + geom_sf(aes(fill = as.numeric(TOT))) + 
+    scale_fill_viridis_c() + labs(fill = "생활인구")
+  
+  ggsave(paste0(path, '/spopvis_', date_val, tz_val, '.jpg'), g)
+  
+}
+
+## exec 
+api_key = '***'
+date_val = '20210610'
+tz_val = '12'
+
+vis_fun(mapp, date_val, tz_val)
+
+
+
